46d90bf2b71cc0483469c3599d66cef0cd2ef7b7
Guilhem Lacassagne <hectochien@gmail.com>
2024-12-25 22:48:51 +0100

    Modify icons

--- app/src/main/java/fr/uha/ensisa/lacassagne/ultimateherdassistant/ui/navigation/BottomNavItem.kt
+++ app/src/main/java/fr/uha/ensisa/lacassagne/ultimateherdassistant/ui/navigation/BottomNavItem.kt
@@ -2,12 +2,12 @@ package fr.uha.ensisa.lacassagne.ultimateherdassistant.ui.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Pets
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class BottomNavItem(val route: String, val icon: ImageVector, val title: String) {
     object Home : BottomNavItem("main_screen", Icons.Default.Home, "Home")
-    object Profile : BottomNavItem("animalList", Icons.Default.Person, "Profile")
-    object AddAnimal : BottomNavItem("addAnimal", Icons.Default.Settings, "Add")
+    object Profile : BottomNavItem("animalList", Icons.Default.Pets, "Profile")
+    object AddAnimal : BottomNavItem("addAnimal", Icons.Default.Add, "Add")
 }
